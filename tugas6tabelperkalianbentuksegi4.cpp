14bfc936009063482f97e376e58dfc6a81368d5f
Mohamad Bagoes Ali Yuddin <72675355+MohamadBagoesAliYuddin@users.noreply.github.com>
2020-11-04 20:39:33 +0700

    Add files via upload

---
+++ tugas6tabelperkalianbentuksegi4.cpp
@@ -0,0 +1,42 @@
+/*NAMA  : MOHAMAD BAGOES ALI YUDDIN  */
+/*NIM   : 20051397048                */
+/*KELAS : MI_2020B                   */
+
+#include<iostream>
+#include<conio.h>
+#include<iomanip>
+using namespace std;
+main(){
+
+int i,j;
+cout <<"======================================================="<< endl;
+cout <<"======================================================="<< endl;
+cout <<"==                                                   =="<< endl;
+cout <<"==   ---------------TABEL PERKALIAN---------------   =="<< endl;
+cout <<"==                                                   =="<< endl;
+cout <<"======================================================="<< endl;
+cout <<"======================================================="<< endl << endl;
+
+for (i=1;i<=10;i++)
+{ cout<<setw(5)<<i;
+
+for (j=1;j<=10;j++)
+cout<<setw(5)<<i*j;
+cout<<endl;
+
+
+}
+cout <<endl <<"======================================================="<< endl;
+	cout <<   "======================================================="<< endl;
+	cout <<   "==                                                   =="<< endl;
+	cout <<   "==                                                   =="<< endl;
+	cout <<   "==                                                   =="<< endl;
+	cout <<   "==             MOHAMAD BAGOES ALI YUDDIN             =="<< endl;
+	cout <<   "==                    20051397048                    =="<< endl;
+	cout <<   "==                                                   =="<< endl;
+	cout <<   "==                                                   =="<< endl;
+	cout <<   "==                                         Penk_Zpk  =="<< endl;
+	cout <<   "======================================================="<< endl;
+	cout <<   "======================================================="<< endl;
+getch();
+}
